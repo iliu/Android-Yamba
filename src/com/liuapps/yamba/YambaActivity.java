e1d87c0842cdb1594dcf500bc93bba6064197b6f
Isaac Liu <liu.isaac@gmail.com>
2011-09-01 22:07:08 -0700

    checked in classpath, android project file. This is version 1 of yamba, single threaded with only status activity

--- src/com/liuapps/yamba/YambaActivity.java
+++ src/com/liuapps/yamba/YambaActivity.java
@@ -1,13 +1,38 @@
 package com.liuapps.yamba;
 
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class YambaActivity extends Activity {
+public class YambaActivity extends Activity implements OnClickListener{
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.status);
+        
+        editText = (EditText) findViewById(R.id.editText);
+        updateButton = (Button) findViewById(R.id.buttonUpdate);
+        
+        updateButton.setOnClickListener(this);
+        
+        twitter = new Twitter("student", "password");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
     }
+    
+    public void onClick(View w) {
+    	twitter.setStatus(editText.getText().toString());
+    	Log.d(TAG, "onClicked");
+    }
+    
+    
 }
