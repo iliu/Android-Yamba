273c61c75c9c2ef375caa9f7d8e9444815ed03bd
Isaac Liu <liu.isaac@gmail.com>
2011-09-05 18:27:48 -0700

    changed the timeline activity to use listView and adapters

--- gen/com/liuapps/yamba/R.java
+++ gen/com/liuapps/yamba/R.java
@@ -15,19 +15,23 @@ public final class R {
         public static final int statusupdatebackground=0x7f020001;
     }
     public static final class id {
-        public static final int buttonUpdate=0x7f070002;
-        public static final int editText=0x7f070001;
-        public static final int itemPrefs=0x7f070008;
-        public static final int itemPurge=0x7f070007;
-        public static final int itemStatus=0x7f070004;
-        public static final int itemTimeline=0x7f070005;
-        public static final int itemToggleService=0x7f070006;
-        public static final int textCount=0x7f070000;
-        public static final int textTimeline=0x7f070003;
+        public static final int buttonUpdate=0x7f070005;
+        public static final int editText=0x7f070004;
+        public static final int itemPrefs=0x7f07000b;
+        public static final int itemPurge=0x7f07000a;
+        public static final int itemStatus=0x7f070007;
+        public static final int itemTimeline=0x7f070008;
+        public static final int itemToggleService=0x7f070009;
+        public static final int listTimeline=0x7f070006;
+        public static final int textCount=0x7f070003;
+        public static final int textCreatedAt=0x7f070001;
+        public static final int textText=0x7f070002;
+        public static final int textUser=0x7f070000;
     }
     public static final class layout {
-        public static final int status=0x7f030000;
-        public static final int timeline=0x7f030001;
+        public static final int row=0x7f030000;
+        public static final int status=0x7f030001;
+        public static final int timeline=0x7f030002;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
--- src/com/liuapps/yamba/TimelineActivity.java
+++ src/com/liuapps/yamba/TimelineActivity.java
@@ -3,21 +3,31 @@ package com.liuapps.yamba;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TimelineActivity extends BaseActivity {
-	private static final String TAG = "StatusActivity";
-	TextView textTimeline;
+	private static final String TAG = "TimelineActivity";
+	static final String[] FROM = { DbHelper.C_CREATED_AT, DbHelper.C_USER,
+	      DbHelper.C_TEXT };  //
+	static final int[] TO = { R.id.textCreatedAt, R.id.textUser, R.id.textText };
+	ListView listTimeline;
+	SimpleCursorAdapter adapter;
 	Cursor cursor;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate");
 		setContentView(R.layout.timeline);
 
-		textTimeline = (TextView) findViewById(R.id.textTimeline);
+		listTimeline = (ListView) findViewById(R.id.listTimeline);
 		
 		// check to see if preferences is set, if not, redirect user to preference screen first
 		if (yamba.getPrefs().getString("username", null) == null) {
@@ -36,12 +46,27 @@ public class TimelineActivity extends BaseActivity {
 		cursor = yamba.getStatusData().getStatusUpdates();
 		startManagingCursor(cursor);
 		
-		String user, text, output;
-		while (cursor.moveToNext()) {
-			user = cursor.getString(cursor.getColumnIndex(DbHelper.C_USER));
-			text = cursor.getString(cursor.getColumnIndex(DbHelper.C_TEXT));
-			output = String.format("%s: %s\n", user, text);
-			textTimeline.append(output);
-		}
+		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO);
+		adapter.setViewBinder(VIEW_BINDER);  
+	    listTimeline.setAdapter(adapter);
 	}
+	
+	// View binder constant to inject business logic that converts a timestamp to
+	  // relative time
+	  static final ViewBinder VIEW_BINDER = new ViewBinder() { // 
+
+	    public boolean setViewValue(View view, Cursor cursor, int columnIndex) { // 
+	      if (view.getId() != R.id.textCreatedAt)
+	        return false; // 
+
+	      // Update the created at text to relative time
+	      long timestamp = cursor.getLong(columnIndex); // 
+	      CharSequence relTime = DateUtils.getRelativeTimeSpanString(timestamp);
+	      Log.d(TAG, relTime.toString());// 
+	      ((TextView) view).setText(relTime); // 
+
+	      return true; // 
+	    }
+
+	  };
 }
